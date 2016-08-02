fca8ed91bd27d1bfeef1138b3ad59f2afa18cb2a
khatwachandan5 <khatwachandan5@gmail.com>
2016-08-03 02:00:48 +0530

    read operation complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 angular.module('MyApp', ['ngMaterial'])
 .controller('AppCtrl', ['$scope','$http',function($scope,$http) {
   $scope.IsVisible = false;
+
   $scope.showAll=function(){
     $scope.IsVisible = true;
 
