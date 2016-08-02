cf7a24eff046647b31ca2430f6cf9a1d5afde33c
khatwachandan5 <khatwachandan5@gmail.com>
2016-08-03 01:34:44 +0530

    dataReadCompleted

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 angular.module('MyApp', ['ngMaterial'])
-.controller('AppCtrl', function($scope) {
-});
+.controller('AppCtrl', ['$scope','$http',function($scope,$http) {
+  $scope.IsVisible = false;
+  $scope.showAll=function(){
+    $scope.IsVisible = true;
+
+    var request=$http.get('http://localhost:8081/data').then(function(response){
+    
+      $scope.employees=response.data;
+    
+    });
+
+  };
+
+}]);
